766fd5e616fbf2d6e0bce762a0abc8201caf545b
yungdaub <100184278@student.burnley.ac.uk>
2025-01-28 14:33:19 +0000

    Push

--- src/Logbook/Week1/Main.java
+++ src/Logbook/Week1/Main.java
@@ -1,8 +1,16 @@
 package Logbook.Week1;
-
+import java.util.Scanner;
 public class Main {
+static void World(){
 
+    System.out.println("Hello world");
+
+}
         public static void main(String[] args) {
-            System.out.println("Hello World");
+            Main.World();
+            System.out.println("What is your name?");
+            Scanner name = new Scanner(System.in);
+            String personname = name.next();
+            System.out.println("Hello "+  personname);
         }
     }
