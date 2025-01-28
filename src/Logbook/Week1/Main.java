1006aec1498dfc415f02de1fcc3bf44431b52e52
yungdaub <100184278@student.burnley.ac.uk>
2025-01-28 14:35:36 +0000

    Push

--- src/Logbook/Week1/Main.java
+++ src/Logbook/Week1/Main.java
@@ -1,7 +1,7 @@
 package Logbook.Week1;
 import java.util.Scanner;
 public class Main {
-static void World(){
+static void World(){ // World function will call the system to print "Hello World"
 
     System.out.println("Hello world");
 
@@ -11,6 +11,6 @@ static void World(){
             System.out.println("What is your name?");
             Scanner name = new Scanner(System.in);
             String personname = name.next();
-            System.out.println("Hello "+  personname);
+            System.out.println("Hello "+  personname); //This is the code for the second task, System will say Hello (userinput)
         }
     }
