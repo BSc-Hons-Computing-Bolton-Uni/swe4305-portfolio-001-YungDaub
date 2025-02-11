9130ebd663084edc8e7895d4d12da644b5888e2b
yungdaub <100184278@student.burnley.ac.uk>
2025-02-11 15:02:54 +0000

    Push

--- src/Logbook/Week2/Week2.java
+++ src/Logbook/Week2/Week2.java
@@ -3,8 +3,9 @@ package Logbook.Week2;
 import java.time.Year;
 import java.util.Scanner;
 
-class Main {
-
+class Main
+{
+//task 2
     static void birthYear(Scanner scanner) {
 
         int currentYear = Year.now().getValue();
@@ -12,15 +13,63 @@ class Main {
         int age = scanner.nextInt();
         int birthYear = currentYear - age;
         System.out.println("Your year of birth " + birthYear);
-        if (age > 18){ // Additional message if the user is over 18
+        if (age > 18) { // Additional message if the user is over 18
             System.out.println(" You are over 18 years old");
+
+
+        }
+    }
+    static void uniGrades(Scanner scanner) {
+        System.out.print ("What is your original grade (Between A-F)");
+
+        String ogGrade = scanner.next();
+        String newGrade = "";
+        switch(ogGrade) {
+            case "A":
+                newGrade = "First";
+                break;
+            case "B":
+                newGrade = "2:1";
+                break;
+            case "C":
+                newGrade = "2:2";
+                break;
+            case "D":
+                newGrade = "3rd";
+                break;
+            case "E":
+                newGrade = "Ordinary";
+                break;
+            case "F":
+                newGrade = "Fail";
+                break;
+
+            default:
+                System.out.print("Please enter a character within the specified range");
+                break;
+
         }
 
+        System.out.print("Your converted grade is " + newGrade);
+
+
+
+
+
+
+
+
+
+
+
     }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         birthYear(scanner);
+        uniGrades(scanner);
+
 
     }
 }
