ac0460fa59b12445a86ad18093e5553fbfca330b
yungdaub <100184278@student.burnley.ac.uk>
2025-02-19 17:38:42 +0000

    Push

--- src/Logbook/Week2/Week2.java
+++ src/Logbook/Week2/Week2.java
@@ -100,7 +100,54 @@ class Main
 
     }
 
+static void tableMessage(Scanner scanner) {
+    char choice;
+    do { //Add in do while loop to the same code as seen in the prior task
+        System.out.print("Enter a number to display its multiplication table");
+        int number = scanner.nextInt();
+        System.out.println("The multiplication table for" + number);
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(i + " x " + number + " = " + (i * number));
 
+
+                /* Here I will add in the functionality which asks
+                if the user would like to continue or exit
+                 */
+        }
+            System.out.print("Would you like to enter another number?");
+        // Ask the user if they want to another number
+        choice = scanner.next().toUpperCase().charAt(0); // Read whether the user wants to run it again
+
+    } while (choice == 'Y') ;
+
+    System.out.print(" Goodbye, thank you for using this system");
+    scanner.close();
+}
+
+    static void examGrader(Scanner scanner) { /*
+    The purpose of this code is to ask the user what mark they
+    received on their test and then calculate their grade from
+    the entered mark, it is pretty simple and can be done with just
+    a few if else statements which i will do below
+    */
+        System.out.println("Enter your mark from 1-100");
+        int mark = scanner.nextInt();
+        if (mark < 0 || mark > 100) {
+            System.out.println("Invalid entry! Please enter a number between 0 and 100."); // to catch any invalid input
+        } else if (mark >= 70) {
+            System.out.println("Your grade: First (1st)");
+        } else if (mark >= 60) {
+            System.out.println("Your grade: Upper Second  (2:1)");
+        } else if (mark >= 50) {
+            System.out.println("Your grade: Lower Second (2:2)");
+        } else if (mark >= 40) {
+            System.out.println("Your grade: Third (3rd)");
+        } else if (mark >= 35) {
+            System.out.println("Your grade: Ordinary"); // if else statements for each instance
+        } else {
+            System.out.println("Your grade: Fail");
+        }
+    }
 
     public static void main(String[] args)
     {
@@ -110,6 +157,7 @@ class Main
         loopTask();
         multiSeven();
         multiTable(scanner);
-
+        tableMessage(scanner);
+        examGrader(scanner);
     }
 }
